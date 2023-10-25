f7fb1d66ed870d2796dbf9ba5f54028c48c3c2c5
vin-sipoi <vincentsipoi01@gmail.com>
2023-10-25 14:00:02 -0700

    1-print_rev_recursion.c

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -5,9 +5,11 @@
  * @s: The string to be printed
  */
 
-void _print_rev_recursion(char *s){
+void _print_rev_recursion(char *s)
+{
 
-	if(*s){
+	if(*s)
+	{
 		_print_rev_recursion(s + 1);
 		_putchar(*s);
 	}
