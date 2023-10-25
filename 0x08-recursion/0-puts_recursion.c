7d9ab39512ed080eb40d9c9c406712c3d35b15a2
vin-sipoi <vincentsipoi01@gmail.com>
2023-10-25 08:44:03 -0700

    0-puts_recursion.c

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,1 +1,18 @@
+#include "main.h"
+/**
+ * _puts_recursion - function like puts();
+ * @s: input
+ * Return: Always 0 (Success)
+ */
+void _puts_recursion(char *su)
+{
+	if (*su)
+	{
+		_putchar(*su);
+		_puts_recursion(su + 1);
+	}
+
+	else
+		_putchar('\n');
+}
 
