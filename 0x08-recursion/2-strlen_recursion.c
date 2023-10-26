f9309e3c5d2e28066b99bfb65c47c4570427b7b3
vin-sipoi <vincentsipoi01@gmail.com>
2023-10-25 19:49:32 -0700

    2-strlen_recursion.c

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,18 @@
+/**
+ * _strlen_recursion - length of string
+ * @s:string
+ * Return:longit
+ */
+int _strlen_recursion(char *s)
+{
+	int longit = 0;
+
+	if (*s)
+	{
+		longit++;
+		longit += _strlen_recursion(s + 1);
+	}
+
+	return (longit);
+}
+
